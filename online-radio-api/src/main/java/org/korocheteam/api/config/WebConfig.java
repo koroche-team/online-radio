633667929a0e67cd76c17f8f0df189541cfe51ab
Georgy Ruzavin <ruzavin02@mail.ru>
2022-05-30 15:43:30 +0300

    add swagger

--- online-radio-api/src/main/java/org/korocheteam/api/config/WebConfig.java
+++ online-radio-api/src/main/java/org/korocheteam/api/config/WebConfig.java
@@ -3,10 +3,19 @@ package org.korocheteam.api.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("org.korocheteam.api.controllers")
 public class WebConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
--- online-radio-api/src/main/java/org/korocheteam/api/controllers/TestController.java
+++ online-radio-api/src/main/java/org/korocheteam/api/controllers/TestController.java
@@ -1,15 +1,21 @@
 package org.korocheteam.api.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("test controller")
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
+    @ApiOperation(value = "return test")
+    @ApiResponse(code = 200, message = "return test")
     @GetMapping
     public ResponseEntity<Object> test() {
         return ResponseEntity.ok("Test");
