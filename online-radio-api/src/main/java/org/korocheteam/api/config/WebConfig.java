11b20737d420b3e05a6c4aae153a0ea7fa31bc8f
Georgy Ruzavin <ruzavin02@mail.ru>
2022-06-03 13:09:55 +0300

    add jwt support for swagger

--- online-radio-api/src/main/java/org/korocheteam/api/config/WebConfig.java
+++ online-radio-api/src/main/java/org/korocheteam/api/config/WebConfig.java
@@ -8,12 +8,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -25,6 +29,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.globalOperationParameters(jwtToken())
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
@@ -39,5 +44,23 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 				.addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**")
 				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+	private List<Parameter> jwtToken() {
+
+		String jwt = "Bearer {jwt}";
+
+		ParameterBuilder tokenPar = new ParameterBuilder();
+		List<Parameter> pars = new ArrayList<>();
+
+		tokenPar.name("Authorization")
+				.description("jwt authorization")
+				.modelRef(new ModelRef("string"))
+				.parameterType("header")
+				.defaultValue(jwt)
+				.required(false);
+		pars.add(tokenPar.build());
+
+		return pars;
 	}
 }
